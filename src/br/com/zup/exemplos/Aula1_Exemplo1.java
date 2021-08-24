ec28d0496431e650f181682da1112a3c647cef00
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-08-24 14:06:08 -0300

    Exemplo da Aula sobre Java - 24.08

---
+++ src/br/com/zup/exemplos/Aula1_Exemplo1.java
@@ -0,0 +1,17 @@
+package br.com.zup.exemplos;
+
+public class Aula1_Exemplo1 {
+    public static void main(String [] args){
+
+        int idade_do_usuario = 18;
+        String nomeDoUsuario = "Leticia";
+
+        System.out.print("Olá, meu nome é ");
+        System.out.print(nomeDoUsuario);
+        System.out.print(" e minha idade é ");
+        System.out.print(idade_do_usuario);
+        System.out.print(" anos.");
+
+
+    }
+}
