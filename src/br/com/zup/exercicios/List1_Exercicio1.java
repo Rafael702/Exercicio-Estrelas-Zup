a37df550839aa5a7bb82995e83931a171161c54c
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-08-24 16:00:08 -0300

    Exercicios Resolvidos.

---
+++ src/br/com/zup/exercicios/List1_Exercicio1.java
@@ -0,0 +1,8 @@
+package br.com.zup.exercicios;
+//1 - Faça um programa que mostre a mensagem "Alô mundo" na tela.
+
+public class List1_Exercicio1 {
+    public static void main (String [] args){
+        System.out.println("Alô mundo!");
+    }
+}
