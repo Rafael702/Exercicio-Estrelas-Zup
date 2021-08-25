7c20d8c168102a0914b27e33c83d32b3a15685d8
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-08-25 13:49:47 -0300

    Misturando os galões com os litros.

---
+++ src/br/com/zup/exercicios/lista2/List2_Exercicio2.java
@@ -0,0 +1,33 @@
+package br.com.zup.exercicios.lista2;
+
+public class List2_Exercicio2 {
+
+    public static void main(String[] args){
+
+        float metros = 16, tintaLitros = 18, tintaGaloes = 3.6F;
+        int valorLitros = 80, valorTintaGaloes = 25;
+
+        float totalTinta = metros/tintaGaloes, valorTotal;
+        //3,6Litros
+        valorTotal = Math.round(totalTinta * valorTintaGaloes);
+        System.out.println(Math.round(totalTinta)+ " R$" + valorTotal);
+
+        //18 Litros
+        metros = 36;
+
+        totalTinta = metros/tintaLitros;
+        valorTotal = totalTinta * valorLitros;
+
+        System.out.println(Math.round(totalTinta) + " R$" + valorTotal);
+
+        metros = 30;
+        totalTinta = (metros/tintaGaloes/tintaLitros)/10;
+
+        System.out.println(totalTinta);
+
+
+
+
+
+    }
+}
