7292b5603db442118f0832fcb8b0cfeb5f9d0109
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-08-25 13:41:38 -0300

    Exercicios finalizados.

---
+++ src/br/com/zup/exercicios/lista1/List1_Exercicio2.java
@@ -0,0 +1,17 @@
+package br.com.zup.exercicios.lista1;
+//2 - Faça um programa que receba 4 notas bimestrais e mostre a média aritmética
+
+public class List1_Exercicio2 {
+
+    public static void main(String [] args){
+        float nota1 = 10, nota2 = 5, nota3 = 7.5F, nota4 = 8;
+
+        float mediaArit = (nota1 + nota2 + nota3 + nota4)/4;
+
+        System.out.printf("A sua média aritmética é: " + mediaArit);
+
+
+
+
+    }
+}
