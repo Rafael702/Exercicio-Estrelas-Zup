3e8a325c413edfb1f5e220358b48bf3d87ff7038
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-08-30 14:27:39 -0300

    Exercicio Resolvido | Lista 4.Exercicio2

--- src/br/com/zup/exercicios/lista4/List4_Exercicio2.java
+++ src/br/com/zup/exercicios/lista4/List4_Exercicio2.java
@@ -1,2 +1,14 @@
-package br.com.zup.exercicios.lista4;public class List4_Exercicio2 {
+package br.com.zup.exercicios.lista4;
+
+public class List4_Exercicio2 {
+    public static void main(String[] args){
+
+        int n = 100;
+
+        while(n >=1){
+            System.out.println(n);
+            n--;
+        }
+
+    }git
 }
