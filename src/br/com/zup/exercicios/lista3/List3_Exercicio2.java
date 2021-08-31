7bb63202a48e7f02881e59aa48b9632aae43b6a0
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-08-31 15:01:47 -0300

    Exercicio Resolvido| Lista 3.Exercicio2| Alcool e Gasolina

--- src/br/com/zup/exercicios/lista3/List3_Exercicio2.java
+++ src/br/com/zup/exercicios/lista3/List3_Exercicio2.java
@@ -1,4 +1,7 @@
 package br.com.zup.exercicios.lista3;
+
+import java.util.Scanner;
+
 /**2 - Um posto está vendendo combustíveis com a seguinte tabela de descontos:
  Álcool:
  até 20 litros, desconto de 3% por litro
@@ -11,7 +14,49 @@ package br.com.zup.exercicios.lista3;
  sabendo-se que o preço do litro da gasolina é R$ 2,50 e o preço do litro do álcool é R$ 1,90.
  ***/
 public class List3_Exercicio2 {
-    public static void main(String[] args) {
+    public static void main(String[] args){
+       Scanner sc = new Scanner(System.in);
+        String combustivel;
+        double valorGasolina = 2.50;
+        double valorAlcool = 1.90;
+        double qtdCombustivel;
+        double valorPagar;
+        double valorComDesconto;
+        double valorTotal = 0;
 
+
+        System.out.println("Informe o tipo de combustivel que você deseja, sendo A para álcool e G para Gasolina:  ");
+        combustivel = sc.next();
+
+
+        if(combustivel.equals("A")){
+            System.out.println("Informe os litros de Alcool desejado: ");
+            qtdCombustivel = sc.nextDouble();
+            if(qtdCombustivel <= 20){
+                valorPagar = valorAlcool * qtdCombustivel;
+                valorComDesconto = valorPagar * 0.03;
+                valorTotal = valorPagar - valorComDesconto;
+            }else if(qtdCombustivel >= 20){
+                valorPagar = valorAlcool * qtdCombustivel;
+                valorComDesconto = valorPagar * 0.05;
+                valorTotal = valorPagar - valorComDesconto;
+            }
+        }else if(combustivel.equals("G")) {
+            System.out.println("Informe os litros de Gasolina desejado: ");
+            qtdCombustivel = sc.nextDouble();
+            if(qtdCombustivel <= 20){
+                valorPagar = valorGasolina * qtdCombustivel;
+                valorComDesconto = valorPagar * 0.04;
+                valorTotal = valorPagar - valorComDesconto;
+            }else if(qtdCombustivel >= 20){
+                valorPagar = valorGasolina * qtdCombustivel;
+                valorComDesconto = valorPagar * 0.6;
+                valorTotal = valorPagar - valorComDesconto;
+            }
+        }
+
+        System.out.println(valorTotal);
+
+        }
     }
-}
+
