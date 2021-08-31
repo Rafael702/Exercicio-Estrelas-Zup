747c99dab2646d182cdab64741ffd06d8d21e949
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-08-31 15:12:39 -0300

    Exemplo com o laço de repetição While

--- src/br/com/zup/exemplos/Aula5_Exemplo4.java
+++ src/br/com/zup/exemplos/Aula5_Exemplo4.java
@@ -1,2 +1,29 @@
-package br.com.zup.exemplos;public class Aula5_Exemplo4 {
+package br.com.zup.exemplos;
+
+import java.util.Scanner;
+
+public class Aula5_Exemplo4 {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+
+        int qtdNumerosPares = 0;
+        int contador = 1;
+
+        System.out.println("Digite a quantidade de números que você quer digitar: ");
+        double qtdNumeros = sc.nextDouble();
+
+        while (contador <= qtdNumeros){
+
+            System.out.println("Digite o número " + contador + "° :");
+            double valorDigitadoPeloUsuario = sc.nextDouble();
+
+            if(valorDigitadoPeloUsuario % 2 == 0){
+                qtdNumerosPares = qtdNumerosPares + 1;
+            }
+
+            contador++;
+        }
+
+        System.out.println("A quantidade de números pares é: " + qtdNumerosPares);
+    }
 }
