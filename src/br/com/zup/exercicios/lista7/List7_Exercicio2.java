713f58b3ec1e1d1ec07d865b1a81a87fafad72c1
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-08-31 15:30:22 -0300

    Solucionando o Exercicio

--- src/br/com/zup/exercicios/lista7/List7_Exercicio2.java
+++ src/br/com/zup/exercicios/lista7/List7_Exercicio2.java
@@ -1,2 +1,10 @@
-package br.com.zup.exercicios.lista7;public class List7_Exercicio2 {
+package br.com.zup.exercicios.lista7;
+
+import java.util.Scanner;
+
+/**2. Faça um programa que leia números inteiros e imprima sua média.**/
+public class List7_Exercicio2 {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+    }
 }
