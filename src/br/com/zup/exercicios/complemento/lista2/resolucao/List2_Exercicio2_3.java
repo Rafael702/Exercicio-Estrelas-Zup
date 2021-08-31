dca147a1c7bcf084232677efa52b51b5659864a8
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-08-31 15:04:17 -0300

    Exercicio Resolvido, resolução 3|

--- src/br/com/zup/exercicios/complemento/lista2/resolucao/List2_Exercicio2_3.java
+++ src/br/com/zup/exercicios/complemento/lista2/resolucao/List2_Exercicio2_3.java
@@ -12,9 +12,40 @@ package br.com.zup.exercicios.complemento.lista2.resolucao;
 public class List2_Exercicio2_3 {
     public static void main(String [] args){
         double area = 60;
-        double qtDeLitrosNecessario = area/6;
+        double qtdDeLitrosNecessaria = area/6;
+        double valorLata = 80;
+        double valorGalao = 25;
 
-        System.out.println("Você precisa de:");
+        System.out.print("Você precisará de: ");
+
+        double latasDeTintaNecessarias = qtdDeLitrosNecessaria/18;
+        double latasDeTintaInteirasNecessarias = Math.ceil(latasDeTintaNecessarias);
+
+        System.out.println(latasDeTintaInteirasNecessarias + " latas de tintas");
+
+        double galaoDeTintaNecessarios = qtdDeLitrosNecessaria/3.6;
+        double galoesDeTintaInteirosNecessarios = Math.ceil(galaoDeTintaNecessarios);
+
+        System.out.print("Você precisará de: ");
+        System.out.println("Você pagará: R$"+latasDeTintaInteirasNecessarias * valorLata);
+        System.out.println(galoesDeTintaInteirosNecessarios+" galões de tinta");
+        System.out.println("Você pagará: R$"+galoesDeTintaInteirosNecessarios * valorGalao);
+
+        double folga = (0.1 * qtdDeLitrosNecessaria);
+        double quantidadeTotalComFolga = qtdDeLitrosNecessaria + folga;
+
+        double resultadoComLatas = Math.ceil(quantidadeTotalComFolga / 18);
+        double resultadoComGaloes = Math.ceil(quantidadeTotalComFolga / 3.6);
+
+        System.out.println("Você pode comprar " +resultadoComLatas );
+        double valorASerPagoNaLataComFolga = resultadoComLatas*valorLata;
+
+        System.out.println("Você precisará pagar "+valorASerPagoNaLataComFolga);
+
+        double valorASerPagoNoGalaoComFolga = resultadoComGaloes*valorGalao;
+
+        System.out.println("Ou você pode comprar " +resultadoComGaloes);
+        System.out.println("Você precisará pagar "+valorASerPagoNoGalaoComFolga);
 
     }
 
