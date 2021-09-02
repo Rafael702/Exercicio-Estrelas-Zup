ea78d91b84398c8df87568aa68d0c933b0f2d164
Rafael702 <rafaeldealmeida.192001@gmail.com>
2021-09-02 15:19:54 -0300

    Exercicio Resolvido| List6.LevelUp| Exercicio resolvido pelo Rafael Santos.

---
+++ src/br/com/zup/exercicios/lista6/List6_LevelUp.java
@@ -0,0 +1,85 @@
+/**LEVEL UP
+ Crie um programa para gerenciar uma lista de alunos, cada aluno tem nome,
+ telefone e email. No final do programa deve ser exibido a lista de alunos cadastrados.
+ Entrega Mínima: O sistema permite, via terminal, inserir pelo menos 1 aluno em uma lista
+ e retornar o mesmo na tela.
+ Entrega Média: O sistema permite adicionar mais de um aluno e apresenta um menu para decidir
+ se deverá adicionar mais alunos ou encerrar o programa.
+ Entrega Máxima: O Sistema permite excluir um aluno usando como parâmetro o email
+ e não permite inserir um aluno com o email repetido.
+ **/
+package br.com.zup.exercicios.lista6;
+
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Scanner;
+
+
+public class List6_LevelUp {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        int sair = 3;
+        String nomeAluno;
+        String emailAluno;
+
+
+        Map<String,String> alunos = new HashMap<String,String>();
+
+        String todosEmails;
+
+        do{
+            System.out.println("Digite o nome do Aluno: ");
+            nomeAluno = sc.next();
+
+            System.out.println("Digite o e-mail do Aluno: ");
+            emailAluno = sc.next();
+
+            if(sair == 3){
+                alunos.put(emailAluno, nomeAluno);
+            }else if(sair == 0){
+             if(alunos.containsKey(emailAluno)) {
+                 System.out.println("Aluno não cadastrado");
+             }else {
+                 todosEmails = emailAluno;
+                 alunos.put(emailAluno, nomeAluno);
+             }
+            }
+
+            System.out.println("Deseja sair do Programa? Digite: 3. " +
+                    "Para continuar cadastrando alunos, digite: 0");
+            sair = sc.nextInt();
+
+        }while (sair !=3);
+
+        System.out.println("Lista de Alunos cadastrados: ");
+        for (String referencia: alunos.keySet()) {
+          System.out.println("Nome do Aluno: " + alunos.get(referencia) + " | E-mail: " + referencia);
+        }
+
+        System.out.println("----------------------------------------------------------------------------");
+        System.out.println("Deseja excluir algum aluno do sistema?");
+        System.out.println("Digite: 0 - Sim ou 3 - Não: ");
+        sair = sc.nextInt();
+
+        if(sair !=3){
+            do{
+                System.out.println("Exclusão de Alunos do Sistema: ");
+
+                System.out.println("Digite o e-mail do Aluno: ");
+                emailAluno = sc.next();
+
+                alunos.remove(emailAluno);
+
+                System.out.println("Deseja sair do Programa? Digite: 3. " +
+                        "Para continuar cadastrando alunos, digite: 0");
+                sair = sc.nextInt();
+
+            }while (sair !=3);
+        }
+
+        for (String referencia: alunos.keySet()) {
+            System.out.println("Nome do Aluno: " + alunos.get(referencia) + " | E-mail: " + referencia);
+        }
+    }
+}
---
+++ src/br/com/zup/exercicios/lista6/levelup/Resolucao_Exercicio.java
@@ -0,0 +1,2 @@
+package br.com.zup.exercicios.lista6.levelup;public class Resolucao_Exercicio {
+}
